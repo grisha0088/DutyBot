4b7233e64ccdafe1577b5a8da1749943e367f88b
frisha0088 <grisha0088@gmail.com>
2016-02-20 14:31:04 +0600

    Переименовал каталог с приложением

---
+++ DutyBot/Telegram/Classes.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Telegram
+{
+    public class From
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+    }
+
+    public class Chat
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Message
+    {
+        public int message_id { get; set; }
+        public From from { get; set; }
+        public Chat chat { get; set; }
+        public int date { get; set; }
+        public string text { get; set; }
+    }
+
+    public class Result
+    {
+        public int update_id { get; set; }
+        public Message message { get; set; }
+
+    }
+
+    public class updates
+    {
+        public bool ok { get; set; }
+        public List<Result> result { get; set; }
+    }
+}
